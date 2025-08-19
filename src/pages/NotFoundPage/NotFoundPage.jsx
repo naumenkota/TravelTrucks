9c2611a2af561effc8d5bb74cb7bc38b69015e8a
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-08-20 00:58:17 +0300

    add NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,15 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button.jsx";
+import s from "./NotFoundPage.module.css"
 
 export default function NotFoundPage() {
     const nav = useNavigate();
 
       return (
-        <div>
-              <h2>Page is not found</h2>
-              
-              <Button  onClick={() => nav(-1)}>Go Back</Button>
-              
+        <div className={s.wrapper}>
+          <h1 className={s.title}>404</h1>  
+          <h2 className={s.subtitle}>Page not found &#128577;</h2>
+          <Button  onClick={() => nav(-1)}>Go Back</Button>
         </div>
     )
 }
