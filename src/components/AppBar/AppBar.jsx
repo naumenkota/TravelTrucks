ef60a671ee6b5f4d32c11eb366b5db8eff85681a
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-08-20 00:37:05 +0300

    add styles for Nav

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,14 +1,18 @@
 import TravelTrucks from "../../assets/TravelTrucks.svg";
 import Nav from "../Nav/Nav.jsx";
 import { Link } from "react-router-dom";
+import Container from "../Container/Container.jsx";
+import s from "./AppBar.module.css";
 
 export default function AppBar() {
     return (
-        <header>  
-            <Link to="/"> 
+        
+            <header className={s.header}>  
+              <Link to="/"> 
                 <img src={TravelTrucks} alt="TravelTrucks Logo" />
-            </Link>
-            <Nav/>
-        </header>
+             </Link>
+              <Nav/>
+            </header>
+        
     )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,15 @@
 import { NavLink } from "react-router-dom";
+import s from "./Nav.module.css";
+import clsx from "clsx";
 
 export default function Nav() {
+   
+  const active = ({ isActive }) => {return clsx(s.navLink, isActive && s.active)};
+
     return (
-    <>
-      <NavLink to="/">Home</NavLink>
-       <NavLink  to="/catalog">Catalog</NavLink>
-    </>
+    <nav className={s.nav}>
+      <NavLink to="/" className={active}>Home</NavLink>
+      <NavLink  to="/catalog" className={active}>Catalog</NavLink>
+    </nav>
   );
 }
