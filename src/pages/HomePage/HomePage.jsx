d02cfdbe7ed440661e6e990a4b851df556a8b7a2
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-08-20 14:17:59 +0300

    fix HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import Container from "../../components/Container/Container";
 
 
+
 export default function HomePage() {
     return (
         <div className={s.hero}>  
-        <Container>  
+           
+               
         <h1 className={s.title}>Campers of your dreams</h1>
         <p className={s.text}>You can find everything you want in our catalog</p>
             <Link to="/catalog" className={s.link}>
                 View Now
-                </Link>
-                   </Container>   
+                    </Link>
+                     
+                  
             </div>
     )
 }
